e2c2592684e52cec4695aecd4f44c4d8cc4c85d5
XE <XE@XE4109>
2025-02-23 15:32:05 +0300

    Добавьте файлы проекта.

---
+++ ООП/ClassCat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ООП
+{
+    internal class ClassCat
+    {
+        public string Name { get; set; }
+        public int age { get; set; }
+        public string poroda { get; set; }
+       
+    }
+}
---
+++ ООП/Program.cs
@@ -0,0 +1,11 @@
+﻿using ООП;
+
+ClassCat moska = new ClassCat();
+ClassCat busa = new ClassCat();
+
+
+
+moska.age = 5;
+moska.poroda = "bengal";
+busa.age = 15;
+busa.poroda = "sfinks";
